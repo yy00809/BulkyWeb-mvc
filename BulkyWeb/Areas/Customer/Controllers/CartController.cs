a25803d41cc221297a3aceb36bb2f664b6da51d4
yy00809 <58527321+yy00809@users.noreply.github.com>
2023-09-22 14:52:10 +0100

    UpdateDynamicDomanTypo

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -125,7 +125,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
 			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
 			{
-				var domain = Request.Scheme+";//" + Request.Host.Value + "/";
+				var domain = Request.Scheme+"://" + Request.Host.Value + "/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",

b33b43f3a2efeb64fea3a26d69bc0d9802c7fbc2
yy00809 <58527321+yy00809@users.noreply.github.com>
2023-09-22 14:38:18 +0100

    Add Dynamic Domain

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -125,7 +125,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
 			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
 			{
-				var domain = "https://localhost:7230/";
+				var domain = Request.Scheme+";//" + Request.Host.Value + "/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
